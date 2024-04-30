1f4f8f094dd552b011cdf24626df8028d57b585e
Alexandr Khanzhin <forgrace5@gmail.com>
2024-04-30 19:59:16 +0500

    Разобрался где содержится таблица стилей, добавил вывод таблицы стилей в консоль

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@
 using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Spreadsheet;
 using DocumentFormat.OpenXml.Wordprocessing;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace course_project
 {
@@ -33,7 +35,7 @@ namespace course_project
             mainDocumentPart.Document ??= new Document();
             Body body = mainDocumentPart.Document.Body ?? mainDocumentPart.Document.AppendChild(new Body());
 
-            // Блок парсинга документа
+            // Блок парсинга текста документа (не уверен что этот блок нужен будет на релизе, так как стили содержатся в другом месте)
             if (body != null)
             {
                 foreach (var element in body.Elements())
@@ -41,10 +43,38 @@ namespace course_project
                     if (element is Paragraph paragraph)
                     {
                         Console.WriteLine(paragraph.InnerText); // Вывод текста в консоль для проверки работы
-                        TextParser(paragraph); // Запуск обработчика текста
                     }
                 }
             }
+
+            // Проверка на пустоту стилей
+            if (mainDocumentPart is null || mainDocumentPart.StyleDefinitionsPart is null || mainDocumentPart.StylesWithEffectsPart is null)
+            {
+                // StylesWithEffectsPart из за чего то равен null, поэтому пока закоментил эту проверку
+                // throw new ArgumentNullException("MainDocumentPart and/or one or both of the Styles parts is null.");
+            }
+
+            XDocument? styles = null;
+            StylesPart? stylesPart = null;
+
+            // Я пока не знаю какой метод из двух использовать нужно будет, посмотрим позже PS StylesWithEffectsPart из за чего то равен null,
+            // поэтому как использовать пока не разобрался 
+            // stylesPart = mainDocumentPart.StylesWithEffectsPart;
+            stylesPart = mainDocumentPart.StyleDefinitionsPart;
+
+            using (var reader = XmlNodeReader.Create(
+              stylesPart.GetStream(FileMode.Open, FileAccess.Read)))
+            {
+                // Создание XDocument.
+                styles = XDocument.Load(reader);
+            }
+
+            // Вывод стилей
+            IEnumerable<XElement> elements =
+                from el in styles.Elements()
+                select el;
+            foreach (XElement el in elements)
+                Console.WriteLine(el);
 
             // Dispose the document handle. PS Не понимаю что делает эта строчка, но оставил ее
             wordprocessingDocument.Dispose();
